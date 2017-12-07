e6e1f6b8b6e93308265f46bf48305a35a26e6b4b
Alexander Ewetumo <trinoxf@gmail.com>
2017-12-07 01:27:08 +0100

    Add initial main.go

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "flag"
+import (
+	"fmt"
+
+	"github.com/influx6/faux/context"
+	"github.com/influx6/faux/flags"
+)
 
 // consts ...
 const (
@@ -10,8 +15,33 @@ const (
 )
 
 func main() {
-	flag.Parse()
+	flags.Run("box", flags.Command{
+		Name:      "Serve",
+		ShortDesc: "runs a box server on the host.",
+		Desc:      "sets up a box server, turning the system into a box container server",
+		Action: func(ctx context.Context) error {
+			fmt.Println("Welcome to box serve!")
 
+			secport, _ := ctx.Bag().GetInt("secport")
+			fmt.Printf("SecPort: %d\n", secport)
+
+			cmdport, _ := ctx.Bag().GetInt("cmdport")
+			fmt.Printf("CmdPort: %d\n", cmdport)
+			return nil
+		},
+		Flags: []flags.Flag{
+			&flags.IntFlag{
+				Name:    "secport",
+				Default: 34567,
+				Desc:    "Port for security tcp server",
+			},
+			&flags.IntFlag{
+				Name:    "cmdport",
+				Default: 34568,
+				Desc:    "Port for commander tcp server",
+			},
+		},
+	})
 }
 
 // serve intiailizesthe box service and creates appropriate profile and
